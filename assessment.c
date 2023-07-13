1ae3566b1b4f669e0ae0fc62eeaaa9d3987281c7
abhayitt <126190372+abhayitt@users.noreply.github.com>
2023-07-13 13:51:32 +0530

    Update assessment.c

--- assessment.c
+++ assessment.c
@@ -1,87 +1,117 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#define CITY_SIZE 20
-#define SUBURBS 2
-#define TOWNS 2
-#define CORPORATIONS 2
+struct node{
+    char data[100];
+    struct node* left;
+    struct node* right;
+};
 
-typedef struct {
-    char city[CITY_SIZE];
-    char suburbs[SUBURBS][CITY_SIZE];
-    char towns[SUBURBS][TOWNS][CITY_SIZE];
-    char corporations[SUBURBS][TOWNS][CORPORATIONS][CITY_SIZE];
-} CityData;
-
-
-void commonDetail(CityData cityData, const char* corporation1, const char* corporation2) {
-    char city_common[20] = "";
-    char suburb_common[20] = "";
-    char town_common[20] = "";
-
-    strcpy(city_common, cityData.city);
-    
-    for (int i = 0; i < SUBURBS; i++) {
-        for (int j = 0; j < TOWNS; j++) {
-            for (int k = 0; k < CORPORATIONS - 1; k++) {
-                if ((strcmp(cityData.corporations[i][j][k], corporation1) == 0 && strcmp(cityData.corporations[i][j][k + 1], corporation2) == 0) ||
-                    (strcmp(cityData.corporations[i][j][k], corporation2) == 0 && strcmp(cityData.corporations[i][j][k + 1], corporation1) == 0)) {
-                    strcpy(suburb_common, cityData.suburbs[i]);
-                    strcpy(town_common, cityData.towns[i][j]);
-                    break;
-                }
-            }
-        }
+int findPath(struct node* root,char* common){
+    if(root==NULL){
+        return 0;
     }
     
-    
-    
-
-    if (strlen(town_common) > 0) {
-        printf("%s, ", town_common);
+    if(strcmp(root->data,common)==0){
+        printf("%s ",root->data);
+        return 1;
     }
-    if (strlen(suburb_common) > 0) {
-        printf("%s, ", suburb_common);
+    
+    int left = findPath(root->left,common);
+    int right = findPath(root->right,common);
+    
+    if(left!=0 || right!=0){
+        printf("%s ",root->data);
+        return 1;
     }
-    printf("%s\n", city_common);
+    
+    return 0;
+    
+}
+struct node *nodeInit(char* name){
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    
+    strcpy(newNode->data,name);
+    newNode->left = newNode->right = NULL;
+    return newNode;
 }
 
+struct node* findCommonTown(struct node* root, char* corp1, char* corp2){
+        if(root==NULL){
+            return NULL;
+        }
+        
+        if(strcmp(root->data,corp1)==0 || strcmp(root->data,corp2)==0){
+            return root;
+        }
+        
+        struct node* left = findCommonTown(root->left,corp1,corp2);
+        struct node* right = findCommonTown(root->right,corp1,corp2);
+        
+        if(left !=NULL && right!=NULL){
+            return root;
+        }
+        
+        else{
+            if(left!=NULL){
+                return left;
+            }
+            return right;
+        }
+}
 
 int main() {
-    CityData cityData;
+    // struct node*root = NULL;
+    struct node* cityNode = nodeInit("Jaipur");
+    
+    struct node* suburb1Node = nodeInit("North");
+    cityNode->left = suburb1Node;
 
-    printf("Enter the city: ");
-    scanf("%[^\n]%*c", cityData.city);
-
-    for (int i = 0; i < SUBURBS; i++) {
-        printf("Enter the suburb %d: ", i + 1);
-        scanf("%[^\n]%*c", cityData.suburbs[i]);
-
-        for (int j = 0; j < TOWNS; j++) {
-            printf("Enter the town %d in %s: ", j + 1, cityData.suburbs[i]);
-            scanf("%[^\n]%*c", cityData.towns[i][j]);
-
-            for (int k = 0; k < CORPORATIONS; k++) {
-                printf("Enter the corporation %d in %s: ", k + 1, cityData.towns[i][j]);
-                scanf("%[^\n]%*c", cityData.corporations[i][j][k]);
-            }
-        }
-    }
-
-    printf("\n");
-    //Input corporations value
-    char corperation1[10];
-    char corperation2[10];
-
-    printf("Enter the code for corporation 1: ");
-    scanf("%s", corperation1);
-
-    printf("Enter the code for corporation 2: ");
-    scanf("%s", corperation2);
-
-    commonDetail(cityData, corperation1, corperation2);
-
+    struct node* suburb2Node = nodeInit("South");
+    cityNode->right = suburb2Node;
+    
+    struct node* town1 = nodeInit("A");
+    struct node* town2 = nodeInit("B");
+    suburb1Node->left = town1;
+    suburb1Node->right = town2;
+    
+    struct node* town3 = nodeInit("C");
+    struct node* town4 = nodeInit("D");
+    suburb2Node->left = town3;
+    suburb2Node->right = town4;
+    
+    struct node* corp1 = nodeInit("56001");
+    struct node* corp2 = nodeInit("56002");
+    town1->left = corp1;
+    town1->right = corp2;
+    
+    struct node* corp3 = nodeInit("56003");
+    struct node* corp4 = nodeInit("56004");
+    town2->left = corp3;
+    town2->right = corp4;
+    
+    struct node* corp5 = nodeInit("56005");
+    struct node* corp6 = nodeInit("56006");
+    town3->left = corp5;
+    town3->right = corp6;
+    
+    struct node* corp7 = nodeInit("56007");
+    struct node* corp8 = nodeInit("56008");
+    town4->left = corp7;
+    town4->right = corp7;
+    
+    char corporation1[100],corporation2[100];
+    printf("Enter Corporation 1\n");
+    scanf("%[^\n]%*c", corporation1);
+    
+    printf("Enter Corporation 2\n");
+    scanf("%[^\n]%*c", corporation2);
+    
+    struct node* ans = findCommonTown(cityNode,corporation1,corporation2);
+    
+    // printf("%s",ans->data);
+    
+    findPath(cityNode,ans);
     return 0;
 }
-
-
